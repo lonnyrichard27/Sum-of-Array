21fa98da0ef180ad5e5c5d20c75704a833b354e2
Oluwaloni Richard <lonnyrichard77@gmail.com>
2020-01-27 11:21:09 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// function sumArray(){
+//   let sum = 0;
+//   let num = [20,30,44]
+//   for (let i = 0; i < num.length; i++) {
+//     sum = sum + num[i]
+//   }
+// return sum;
+// }
+// sumArray();
+
+function sumAll(arr) {
+  var i;
+  var sum = 0;
+  for(i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum;
+}
+sumAll([29,48,60])
+// anytime you see the keyword "new" anything that comes after is a constructor
+
